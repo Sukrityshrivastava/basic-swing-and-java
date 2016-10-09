1b77b159284a20715139afabbf687bd5030db010
Sukrity Shrivastava <ridhishrivastava691@gmail.com>
2016-10-10 02:24:31 +0530

    Update ShowAll.java

--- ShowAll.java
+++ ShowAll.java
@@ -9,10 +9,6 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author anand
- */
 public class ShowAll extends javax.swing.JFrame {
 
     /**
