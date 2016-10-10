c6717889dc3b0a2ad44a9716305305ba5f4805b5
Sukrity Shrivastava <ridhishrivastava691@gmail.com>
2016-10-10 14:35:58 +0530

    Update EventData.java

--- EventData.java
+++ EventData.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class EventData extends javax.swing.JFrame {
+public class EnterData extends javax.swing.JFrame {
 
     /**
      * Creates new form EnterData
